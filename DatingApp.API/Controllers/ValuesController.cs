b3c257c1e79bea1f31862e106bea74d33b9a5937
husooM <huso.m@hotmail.com>
2018-11-18 00:53:34 +0100

    End of section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -36,6 +38,7 @@ namespace DatingApp.API.Controllers
             return Ok(value);
         }
 
+        [AllowAnonymous]
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
